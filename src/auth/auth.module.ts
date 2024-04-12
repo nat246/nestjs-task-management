96013417e91149850425dbc5501bc429c2b0cc45
Nathanael Ang <natnatang@hotmail.com>
2024-04-12 10:42:44 +1000

    Created User Entity and Repository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRepository } from './users.repository';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [AuthService, UserRepository],
+  controllers: [AuthController],
 })
 export class AuthModule {}
