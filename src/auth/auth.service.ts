0956d137f14ae448d21603134c38244b125da5c6
Nathanael Ang <natnatang@hotmail.com>
2024-04-24 18:34:13 +1000

    Implement sign up functionality in AuthController and AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './users.repository';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersRepository: UserRepository) {}
+
+  async signUp(authCredentialsDto: AuthCredentialDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const newUser = this.create({
+      username,
+      password,
+    });
+
+    await this.save(newUser);
   }
 }
