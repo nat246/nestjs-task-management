0e0181ea0f3bc5fb9151028e565cdc45dd05f8d3
Nathanael Ang <natnatang@hotmail.com>
2024-04-30 11:43:31 +1000

    Refactor sign in functionality in AuthController and AuthService to return JWT access token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,7 +14,9 @@ export class AuthController {
   }
 
   @Post('/signin')
-  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialDto) {
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,33 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './users.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersRepository: UserRepository) {}
+  constructor(
+    private readonly usersRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialDto) {
+  async signIn(
+    authCredentialsDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     const username =
       await this.usersRepository.validateUserPassword(authCredentialsDto);
 
     if (!username) {
       throw new UnauthorizedException('Invalid Credentials');
     }
+
+    const payload: JwtPayload = { username };
+    const accessToken = await this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
